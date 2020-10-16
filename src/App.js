d2d8dfb31f1f5f9551e5be5334b1794e976b7fa6
Lazar <jlazar997@gmail.com>
2020-10-16 15:31:08 +0200

    added apollo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Launches from './components/Launches'
 import './App.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 function App() {
-  return (
-    <div className="App">
-    SpaceX
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div className='App'>SpaceX</div>
+      <Launches/>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Query, gql } from '@apollo/client';
+
+const LAUNCHES_QUERY = gql`
+query LaunchesQuery{
+    launches{
+        flight_number
+        mission_name
+        launch_date_local
+        launch_success
+    }}
+    `;
 
 const Launches = () => {
+	return (
+		<div>
+            <h2>Launches</h2>
+            <Query query={LAUNCHES_QUERY}>
+            {
+                ({loading, error, data})=> {
+                    if(loading) return <h4>Loading...</h4>
+                    if(error) console.log(error);
+                    console.log(data)
+                    return <h1>test</h1>
 
-    
+                }
+            }
+            </Query>
+		</div>
+	);
 };
 
-export default Launches;
+
+
+export default Launches
